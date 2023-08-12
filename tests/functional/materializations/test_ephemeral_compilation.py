e8d7304f0b490359f9e52603d3b0e451a359be03
Michelle Ark <michelle.ark@dbtlabs.com>
2023-08-11 22:28:48 -0400

    add TestLargeEphemeralCompilation

--- tests/functional/materializations/test_ephemeral_compilation.py
+++ tests/functional/materializations/test_ephemeral_compilation.py
@@ -7,50 +7,22 @@ from dbt.tests.util import run_dbt
 # fails fairly regularly if that is broken, but does occasionally work (depending
 # on the order in which things are compiled). It requires multi-threading to fail.
 
-
-fct_eph_first_sql = """
--- fct_eph_first.sql
-{{ config(materialized='ephemeral') }}
-
-with int_eph_first as(
-    select * from {{ ref('int_eph_first') }}
+from tests.functional.materializations.fixtures import (
+    fct_eph_first_sql,
+    int_eph_first_sql,
+    schema_yml,
+    bar_sql,
+    bar1_sql,
+    bar2_sql,
+    bar3_sql,
+    bar4_sql,
+    bar5_sql,
+    baz_sql,
+    baz1_sql,
+    foo_sql,
+    foo1_sql,
+    foo2_sql,
 )
-
-select * from int_eph_first
-"""
-
-int_eph_first_sql = """
--- int_eph_first.sql
-{{ config(materialized='ephemeral') }}
-
-select
-    1 as first_column,
-    2 as second_column
-"""
-
-schema_yml = """
-version: 2
-
-models:
-  - name: int_eph_first
-    columns:
-      - name: first_column
-        tests:
-          - not_null
-      - name: second_column
-        tests:
-          - not_null
-
-  - name: fct_eph_first
-    columns:
-      - name: first_column
-        tests:
-          - not_null
-      - name: second_column
-        tests:
-          - not_null
-
-"""
 
 
 class TestEphemeralCompilation:
@@ -69,3 +41,28 @@ class TestEphemeralCompilation:
 
         results = run_dbt(["test"])
         len(results) == 4
+
+
+# From: https://github.com/jeremyyeo/ephemeral-invalid-sql-repro/tree/main/models
+class TestLargeEphemeralCompilation:
+    @pytest.fixture(scope="class")
+    def models(self):
+
+        return {
+            "bar.sql": bar_sql,
+            "bar_1.sql": bar1_sql,
+            "bar_2.sql": bar2_sql,
+            "bar_3.sql": bar3_sql,
+            "bar_4.sql": bar4_sql,
+            "bar_5.sql": bar5_sql,
+            "baz.sql": baz_sql,
+            "baz_1.sql": baz1_sql,
+            "foo.sql": foo_sql,
+            "foo_1.sql": foo1_sql,
+            "foo_2.sql": foo2_sql,
+        }
+
+    def test_ephemeral_compilation(self, project):
+        # 8/11 table models are built as expected. no compilation errors
+        results = run_dbt(["compile"])
+        assert len(results) == 8
